f06e3e1443098aed90b03d15c62288e446bda8a7
QA Hoang <qahoang317@gmail.com>
2024-12-26 18:02:31 +0700

    [database] added more db setup queries

--- init-scripts/db_setup.sql
+++ init-scripts/db_setup.sql
@@ -12,4 +12,36 @@ create table if not exists reservations (
     num_guests INT NOT NULL
 );
 
-create index idx_reservation_dates on reservations (start_date, end_date);
+-- create index idx_reservation_dates on reservations (start_date, end_date);
+
+CREATE TABLE calendar (
+    date DATE NOT NULL PRIMARY KEY,
+    is_available BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+WITH RECURSIVE date_generator AS (
+    SELECT '2024-01-01' AS date
+    UNION ALL
+    SELECT DATE_ADD(date, INTERVAL 1 DAY)
+    FROM date_generator
+    WHERE date < '2034-12-31'
+)
+INSERT INTO calendar (date)
+SELECT date FROM date_generator;
+
+-- Insert test data
+INSERT INTO reservations (
+  email, first_name, last_name, national_id, start_date, end_date, num_guests
+) VALUES
+('john.doe@example.com', 'John', 'Doe', 'ABC123', '2024-12-25', '2024-12-31', 2),
+('jane.smith@example.com', 'Jane', 'Smith', 'XYZ789', '2025-01-01', '2025-01-03', 4),
+('michael.brown@example.com', 'Michael', 'Brown', 'LMN456', '2025-01-04', '2025-01-05', 1),
+('linda.jones@example.com', 'Linda', 'Jones', 'QRS987', '2025-01-06', '2025-01-31', 3);
+
+UPDATE calendar SET is_available = FALSE
+WHERE TRUE
+AND DATE between '2024-12-25' AND '2024-12-31'
+AND DATE between '2025-01-01' AND '2025-01-03'
+AND DATE between '2025-01-04' AND '2025-01-05'
+AND DATE between '2025-01-06' AND '2025-01-31'
+;
